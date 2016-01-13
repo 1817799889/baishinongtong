298b8c6718fdd6c315e6a6628f5d296ae5c822a9
cjw <1593407336@qq.com>
2016-01-13 09:05:38 +0800

    first commit

---
+++ BSNT_Android_TV/src/main/java/com/cn/bsnt/service/GoodsAgriculturesMachineryService.java
@@ -0,0 +1,13 @@
+package com.cn.bsnt.service;
+
+import java.util.List;
+
+import com.cn.bsnt.model.GoodsAgriculturesMachinery;
+
+
+
+public interface GoodsAgriculturesMachineryService {
+
+	public List<GoodsAgriculturesMachinery> selectAll();
+	public List<GoodsAgriculturesMachinery> selectAllById();
+}
