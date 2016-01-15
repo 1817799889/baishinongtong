6d412bbf808a62a5a310348a29a7420201aae30f
cjw <1593407336@qq.com>
2016-01-15 15:51:35 +0800

    c

---
+++ BSNT_Android_TV/src/main/java/com/cn/bsnt/goodsAgriculturesUseTools/GoodsAgriculturesUseToolsAction.java
@@ -0,0 +1,49 @@
+package com.cn.bsnt.goodsAgriculturesUseTools;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+import dbUtil.DBConntext;
+
+@SuppressWarnings("serial")
+public class GoodsAgriculturesUseToolsAction extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//…Ë÷√±‡¬Î∏Ò Ω
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		DBConntext db = new DBConntext();
+		JSONObject json = new JSONObject();
+		List<Map<String, Object>> list_rtn = new ArrayList<Map<String,Object>>();
+		List<Map<String, Object>> list_picture = new ArrayList<Map<String,Object>>();
+		String goodsType=request.getParameter("");
+		List<String> list= new ArrayList<String>();
+		list.add(goodsType);
+		list_rtn = db.queryUseTools();
+		list_picture = db.queryPicture(list);
+		String state = "-1";
+		if (list_rtn != null && list_rtn.size()>0) {
+			state = "1";
+		}else {
+			state = "0";
+		}
+		json.put("state", state);
+		json.put("agriculturesUseToolsList", list_rtn);
+		json.put("list_picture", list_picture);
+		response.getWriter().write(json.toString());
+
+	}
+			
+}
---
+++ BSNT_Android_TV/src/main/java/com/cn/bsnt/goodsComputer/GoodsComputerAction.java
@@ -0,0 +1,71 @@
+package com.cn.bsnt.goodsComputer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+import dbUtil.DBConntext;
+
+@SuppressWarnings("serial")
+public class GoodsComputerAction extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		DBConntext db = new DBConntext();
+		JSONObject json = new JSONObject();
+		List<Map<String, Object>> list_rtn = new ArrayList<Map<String,Object>>();
+		List<Map<String, Object>> list_picture = new ArrayList<Map<String,Object>>();
+		String goodsType=request.getParameter("");
+		List<String> list= new ArrayList<String>();
+		list.add(goodsType);
+		list_rtn = db.queryComputer();
+		list_picture = db.queryPicture(list);
+		String state = "-1";
+		if (list_rtn != null && list_rtn.size()>0) {
+			state = "1";
+		}else {
+			state = "0";
+		}
+		json.put("state", state);
+		json.put("computerList", list_rtn);
+		json.put("list_picture", list_picture);
+		response.getWriter().write(json.toString());
+		
+	}
+
+}
